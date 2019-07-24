d4246ed7161efd879ea6154d9ac2d190470a5587
RubaSathish <53251305+RubaSathish@users.noreply.github.com>
2019-07-24 15:15:06 +0530

    Create ruba1.c

---
+++ ruba1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n1,n2;
+    scanf("%d%d",&n1,&n2);
+    for(i=n1+1;i<n2;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d",i);
+        }
+    }
+
+    return 0;
+}
